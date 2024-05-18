fb6cbe3b5284929196dcd635efe5c1dbaec5e504
Nithish <madanithish6606@gmail.com>
2024-05-18 22:13:04 +0530

    regex

--- bomb.py
+++ bomb.py
@@ -1,13 +1,15 @@
+import re
 from _ import api
 def main()->None:
-	target:str=str(input("Enter victim ph.no:"))
-	if len(target)==10:
-		print("+91"+target)
-	elif len(target)!=10:
-		print("Invalid number")
-		exit()
+	target:str=str(input("Enter victim ph.no(Ex:+919876543210):"))
+
+	pattern = re.compile(r'^\+91\d{10}$')
+
+	if pattern.match(target):
+		print(target)
 	else:
 		print("Please enter correct number")
+		return
 
 	max:int=10**500
 	msg_count:int=int(input("Enter msg count(enter 0 for unlimited sms):"))
